0350957f70b51dfdb5dc3ac5f0992b5d463843f4
Bilal Khan <bk@tinymanager.com>
2020-02-21 14:52:07 -0600

    Edit dataset to cound n_original_tokens correctly

--- finetune.py
+++ finetune.py
@@ -59,8 +59,8 @@ class TextDataset(Dataset):
         with open(path, encoding="utf-8") as handle:
             if args.efficient or args.fast:
                 for line in handle:
+                    self.n_original_tokens += len(line.strip().split(" "))
                     if len(line) > 0 and not line.isspace():
-                        self.n_original_tokens += len(line.strip().split(" "))
                         text.append(line)
             else:
                 temp = handle.read()
