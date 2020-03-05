a364bc6087c8a6b6bc13db0a61fabf6126184fd3
Bilal Khan <bk@tinymanager.com>
2020-03-05 08:44:39 -0600

    Temp remove grad clipping

--- finetune_pl.py
+++ finetune_pl.py
@@ -364,7 +364,7 @@ if __name__ == "__main__":
     model = LM(args)
     trainer = pl.Trainer(max_epochs=args.epochs, accumulate_grad_batches=args.grad_steps, gpus=args.n_gpus, num_tpu_cores=args.n_tpu_cores,
                          precision=args.precision, amp_level=args.apex_mode, resume_from_checkpoint=args.checkpoint, logger=wandb_logger, fast_dev_run=args.debug_run, early_stop_callback=early_stopping_callback, checkpoint_callback=checkpoint_callback,
-                         test_percent_check=0, progress_bar_refresh_rate=1, gradient_clip_val=1)
+                         test_percent_check=0, progress_bar_refresh_rate=1)
 
     if args.run_eval:
         trainer.test(model)
